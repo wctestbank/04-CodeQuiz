59aca1fc28c51f93e173d3e0898f546c1803c041
wctestbank <wc.businessmail@gmail.com>
2022-04-28 20:07:29 -0400

    populate questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,24 @@ var timerInstances = 0;
 var introEl = document.querySelector("#intro");
 var questionEl = document.querySelector("#question");
 
+// array of questions
+
+var questionPool =
+    [
+        {
+            question: "Test",
+            option1: "Test 1",
+            option2: "Test 2",
+            option3: "Test 3",
+            option4: "Test 4",
+            correct: 1
+        }
+    ];
+
+// tracker for current question in pool
+var currentQuestion = 0;
+
+
 // countdown timer
 var timerCountdown = function () {
 
@@ -35,10 +53,42 @@ var timerCountdown = function () {
 
 };
 
-var displayQuiz = function (event) {
+// switches to quiz from intro
+var startQuiz = function () {
     introEl.hidden = true;
     questionEl.hidden = false;
     timerCountdown();
+
+    currentQuestion = randomQuestion();
+
+    populateQuestion(currentQuestion);
+
 };
 
-timerEl.addEventListener("click", displayQuiz)
+// main handling of quiz
+var quizOperation = function (event) {
+
+    if (event.target.value) {
+        var userAnswer = event.target.value;
+        console.log(userAnswer);
+    }
+};
+
+var randomQuestion = function () {
+    return Math.floor(Math.random() * questionPool.length);
+};
+
+var populateQuestion = function (questionNumber) {
+
+    // populates question fields with current question
+    questionEl.querySelector("#questionText").textContent = questionPool[questionNumber].question;
+
+    questionEl.querySelector("#a1").textContent = questionPool[questionNumber].option1;
+    questionEl.querySelector("#a2").textContent = questionPool[questionNumber].option2;
+    questionEl.querySelector("#a3").textContent = questionPool[questionNumber].option3;
+    questionEl.querySelector("#a4").textContent = questionPool[questionNumber].option4;
+
+};
+
+timerEl.addEventListener("click", startQuiz);
+questionEl.addEventListener("click", quizOperation);
